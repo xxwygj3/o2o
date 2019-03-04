475a5306a66dadb137372205ed717139cc494681
xxwygj3 <xxwygj3@126.com>
2019-03-04 17:03:49 +0800

    ssm配置与jar包

---
+++ src/main/java/com/testssm/o2o/dao/AreaDao.java
@@ -0,0 +1,13 @@
+package com.testssm.o2o.dao;
+
+import com.testssm.o2o.entity.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+    /**
+     * 列出区域列表
+     * @return areaList
+     */
+    List<Area> queryArea();
+}
