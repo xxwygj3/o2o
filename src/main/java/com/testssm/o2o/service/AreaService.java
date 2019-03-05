4c96fac848ad8d7ae770c4c366deec22f826be23
xxwygj3 <xxwygj3@126.com>
2019-03-05 17:03:18 +0800

    areaList,controller,service,dao

---
+++ src/main/java/com/testssm/o2o/service/AreaService.java
@@ -0,0 +1,10 @@
+package com.testssm.o2o.service;
+
+import com.testssm.o2o.entity.Area;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface AreaService {
+    List<Area> getAreaList();
+}
